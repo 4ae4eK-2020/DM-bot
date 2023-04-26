9882105b1ce4ff8a6eb7771126c85b5ccc485874
4ae4eK-2020 <67694568+4ae4eK-2020@users.noreply.github.com>
2023-04-26 20:54:15 +0500

    DB connect

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import datetime
 import discord
 from discord.ext import commands
 from discord.ui import Button, View
 import disnake
 from dotenv import load_dotenv
+from model import *
 import os
 
 load_dotenv()
@@ -32,55 +32,67 @@ async def hello(message):
         return
     await message.send("Привет, " + message.author.name)
 
-
-@bot.command()
-async def giveRole(ctx, *, roleName):
-    print(roleName)
-    guild = ctx.guild
-    user = ctx.message.author
-    role = discord.utils.get(guild.roles, name=roleName)
-    if role:
-        await user.add_roles(role)
-    else:
-        await guild.create_role(name=roleName)
-        await user.add_roles(user, role)
-
-
-@bot.command()
-async def newGame(ctx, title, *, args):
-    embed = disnake.Embed(
-        title=title,
-        description=args,
-        color=disnake.Colour.dark_green()
-    )
-
-    accept = Button(label="Принять участие", style=discord.ButtonStyle.secondary, emoji="✅")
-    cancel = Button(label="Подписаться", style=discord.ButtonStyle.secondary, emoji="❌")
-    view = View()
-    view.add_item(accept)
-    view.add_item(cancel)
-    await ctx.send(embed=embed, view=view)
+with db:
+    tables = [Place, Event, User]
+    if not all(table.table_exists() for table in tables):
+        db.create_tables(tables)
+    @bot.command()
+    async def giveRole(ctx, *, roleName):
+        print(roleName)
+        guild = ctx.guild
+        user = ctx.message.author
+        role = discord.utils.get(guild.roles, name=roleName)
+        if role:
+            await user.add_roles(role)
+        else:
+            await guild.create_role(name=roleName)
+            await user.add_roles(user, role)
 
 
-@bot.command()
-async def newChannel(ctx, channelName):
-    guild = ctx.guild
-    category = discord.utils.get(guild.categories, name="Лобби")
+    @bot.command()
+    async def newGame(ctx, *, args):
 
-    embed = disnake.Embed(
-        title="Успех!",
-        description=f"Канал {channelName} был успешно создан",
-        color=disnake.Colour.dark_green(),
-    )
+        args_arr = args.split(';')
+        title = args_arr[0]
 
-    await guild.create_text_channel(name=channelName, category=category)
-    await ctx.send(embed=embed)
+        args_arr[0] = ''
+
+        embed = disnake.Embed(
+            title=title,
+            description=''.join(args_arr),
+            color=disnake.Colour.dark_green()
+        )
+
+        accept = Button(label="Принять участие", style=discord.ButtonStyle.secondary, emoji="✅", custom_id="participate")
+        cancel = Button(label="Подписаться", style=discord.ButtonStyle.secondary, emoji="🔔", custom_id="subscription")
+        view = View()
+        view.add_item(accept)
+        view.add_item(cancel)
+        await ctx.send(embed=embed, view=view)
 
 
-@bot.command()
-async def newPlayer(ctx, *, message):
-    # do something with DB
-    print("1")
+    @bot.command()
+    async def newChannel(ctx, channelName):
+        guild = ctx.guild
+        category = discord.utils.get(guild.categories, name="Лобби")
 
+        embed = disnake.Embed(
+            title="Успех!",
+            description=f"Канал {channelName} был успешно создан",
+            color=disnake.Colour.dark_green(),
+        )
+
+        await guild.create_text_channel(name=channelName, category=category)
+        await ctx.send(embed=embed)
+
+    @bot.listen("on_button_click")
+    async def game_listener(inter: disnake.MessageInteraction):
+        emoji_id = inter.component.custom_id
+        if emoji_id not in ["participate", "subscription"]:
+            return
+
+       # if emoji_id == "participate":
+            #role = discord.utils.get(inter.guild.roles, name=)
+         #   await inter.guild.get_role()
 
 bot.run(os.getenv("TOKEN"))
